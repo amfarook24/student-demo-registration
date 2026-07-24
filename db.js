9e2b51a14e826051c8bdbbb4167a5c2ae263d28d
amfarook24 <116320522+amfarook24@users.noreply.github.com>
2026-07-23 20:43:43 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,48 @@
-const mysql = require("mysql2");
+CREATE DATABASE IF NOT EXISTS student_demo;
 
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+USE student_demo;
 
-db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:");
-        console.log(err);
-    } else {
-        console.log("Connected to MySQL!");
-    }
-});
 
-module.exports = db;
+CREATE TABLE timeslots (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    slot_time VARCHAR(100) NOT NULL,
+    capacity INT DEFAULT 6
+);
+
+
+CREATE TABLE students (
+
+    student_id VARCHAR(8) PRIMARY KEY,
+
+    first_name VARCHAR(50) NOT NULL,
+
+    last_name VARCHAR(50) NOT NULL,
+
+    project_title VARCHAR(100) NOT NULL,
+
+    email VARCHAR(100) NOT NULL,
+
+    phone VARCHAR(12) NOT NULL,
+
+    timeslot_id INT NOT NULL,
+
+    FOREIGN KEY (timeslot_id)
+    REFERENCES timeslots(id)
+
+);
+
+
+INSERT INTO timeslots(slot_time, capacity)
+VALUES
+
+('4/19/2070 6:00 PM - 7:00 PM',6),
+
+('4/19/2070 7:00 PM - 8:00 PM',6),
+
+('4/19/2070 8:00 PM - 9:00 PM',6),
+
+('4/20/2070 6:00 PM - 7:00 PM',6),
+
+('4/20/2070 7:00 PM - 8:00 PM',6),
+
+('4/20/2070 8:00 PM - 9:00 PM',6);
